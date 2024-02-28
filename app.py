4b89a3b39988de6280895eff0e1bcfe963aa638b
asynk <asynk@vk.com>
2024-02-28 16:26:53 +0300

    2.1

--- app.py
+++ app.py
@@ -3,8 +3,19 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 
 
+
 app = Flask(__name__)
+mysql = MySQL(app)
 
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "database"
+
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.secret_key='secret123'
+    app.run(debug=True)
